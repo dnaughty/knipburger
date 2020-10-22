6584c8ddd269a40ea9fa91d946e1ee2f98bd140e
Denis Naughton <mjolinor75@hotmail.com>
2020-10-22 18:00:37 -0400

    changed roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root 	'static_pages#menu'
-  get 'static_pages/menu'
-  get 'static_pages/knippenblogger'
-  get 'static_pages/events'
+ 
+  get 'knippenblogger' => 'static_pages#knippenblogger'
+  get 'events' => 'static_pages/events'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
